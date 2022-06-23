1a818c1c1cb950fd22957623243c08d2176b658f
Ethan Mick <ethan@ethanmick.com>
2022-06-22 21:56:07 -0400

    Playing with exercise page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,118 @@
+import { DateTime } from 'luxon'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+
+const exercise = {
+  name: 'Bench Press',
+  final: 'Drop Set',
+  previous: [
+    {
+      reps: 8,
+      weight: 70,
+    },
+    {
+      reps: 8,
+      weight: 70,
+    },
+    {
+      reps: 8,
+      weight: 70,
+    },
+    {
+      reps: 8,
+      weight: 70,
+    },
+  ],
+  current: [
+    {
+      reps: '6-8',
+      weight: 80,
+    },
+  ],
+}
+
+const Sets = () => {
+  return (
+    <div className="bg-neutral-900 text-gray-50 px-4 shadow">
+      <h2 className="text-2xl py-8">Sets</h2>
+      <h3>Previous</h3>
+      <div className="flex gap-16 py-8">
+        <div className="flex flex-col items-start">
+          <div className="text-2xl">4</div>
+          <div className="text-sm font-light uppercase">Sets</div>
+        </div>
+        <div className="flex flex-col items-start">
+          <div className="text-2xl">8</div>
+          <div className="text-sm font-light uppercase">Reps</div>
+        </div>
+        <div className="flex flex-col items-start">
+          <div className="text-2xl">50</div>
+          <div className="text-sm font-light uppercase">lbs</div>
+        </div>
+      </div>
+      <ul>
+        <li className="grid grid-cols-9 gap-8">
+          <div>Set</div>
+          <div className="col-span-4">Reps</div>
+          <div className="col-span-4">Weight</div>
+        </li>
+        {exercise.previous.map((s, key) => (
+          <li key={key} className="grid grid-cols-9 gap-8 py-8">
+            <div>{key + 1}</div>
+            <div className="mt-1 relative rounded-md shadow-sm col-span-4">
+              <input
+                type="number"
+                defaultValue={`${s.reps}`}
+                className="shadow-sm focus:ring-amber-500 focus:border-amber-500 block w-full sm:text-sm border-gray-300 bg-neutral-800"
+              />
+            </div>
+            <div className="mt-1 relative rounded-md col-span-4">
+              <input
+                type="number"
+                defaultValue={s.weight}
+                className="shadow-sm focus:ring-amber-500 focus:border-amber-500 block w-full sm:text-sm border-gray-300 bg-neutral-800"
+              />
+              <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                <span className="text-gray-500 sm:text-sm" id="price-currency">
+                  lbs
+                </span>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+type NameProps = React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLHeadingElement>,
+  HTMLHeadingElement
+>
+
+const Name = ({ children, ...rest }: NameProps) => (
+  <div className="bg-black px-4 py-12 text-white">
+    <h1 className="py-4 font-semibold text-3xl">{children}</h1>
+    <h2>{DateTime.now().minus({ week: 1 }).toRelative()}</h2>
+  </div>
+)
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="bg-neutral-900">
       <Head>
-        <title>Create Next App</title>
+        <title>FitZer0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className="pb-8">
+        <Name>{exercise.name}</Name>
+        <Sets />
+        <div className="px-4">
+          <button className="text-white border w-full py-4 border-neutral-300">
+            Next
+          </button>
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   )
 }
