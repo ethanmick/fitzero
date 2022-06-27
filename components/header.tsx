b97e15c8d9db23044dc750714c27e076f6d9ce84
Ethan Mick <ethan@ethanmick.com>
2022-06-26 21:08:54 -0400

    Back Button updates (#1)

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,4 @@
-import classNames from 'classnames'
-import { PropsWithChildren, ReactElement, ReactNode } from 'react'
+import { ReactElement, ReactNode } from 'react'
 
 type Props = {
   left?: ReactNode
@@ -9,7 +8,7 @@ type Props = {
 
 export const Header = ({ left, right, children }: Props) => (
   <div className="grid grid-cols-8 items-center py-16">
-    <span className="col-span-1 flex justify-start">{left}</span>
+    <span className="col-span-1 flex justify-start h-full">{left}</span>
     <span className="col-span-6">{children}</span>
     <span className="col-span-1 flex justify-end">{right}</span>
   </div>
