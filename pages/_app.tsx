5c12012fee00b6640de633862ca52d3776846e35
Ethan Mick <ethan@ethanmick.com>
2022-06-27 19:53:17 -0400

    PWA Icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
-import Colors from 'tailwindcss/colors'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import Colors from 'tailwindcss/colors'
 import '../styles/globals.css'
 
 const client = new ApolloClient({
@@ -14,14 +14,31 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>FitZero</title>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/favicon.ico" />
+
+        <meta name="application-name" content="FitZero" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="FitZero" />
+        <meta name="description" content="Track your fitness" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="black" />
+        <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content={Colors.neutral['900']} />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
         />
+
+        {/* Icons */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <ApolloProvider client={client}>
         <Component {...pageProps} />
