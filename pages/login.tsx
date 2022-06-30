31175d4f2290f0c66075a398af494689d2027c6b
victorlora <3lorav+github@gmail.com>
2022-06-29 21:38:41 -0400

    🐛 Fix redirect_to url for login/signup

--- pages/login.tsx
+++ pages/login.tsx
@@ -25,7 +25,7 @@ const Login: NextPage = () => {
 
     try {
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_ROOT}/login?redirect_to=${window.location.host}`,
+        `${process.env.NEXT_PUBLIC_API_ROOT}/login?redirect_to=${window.location.origin}`,
         {
           method: 'POST',
           credentials: 'include',
