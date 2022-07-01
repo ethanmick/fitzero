cb054fc09cd6a427423462a6c5200244343403fa
Ethan Mick <ethan@ethanmick.com>
2022-07-01 11:04:39 -0400

    Simple home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,32 @@
 import { Route } from 'lib'
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import Link from 'next/link'
+
+const Logo = () => (
+  <div className="flex justify-center">
+    <Image width={800 / 3} height={160 / 3} src="/logo.svg" alt="FitZero" />
+  </div>
+)
+
+const Temp = () => (
+  <div className="flex justify-center mt-8">
+    <Link
+      href={{
+        pathname: Route.Login,
+      }}
+    >
+      <a className="px-4 py-2 border border-gray-50 text-xl">Login</a>
+    </Link>
+  </div>
+)
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-neutral-900">
-      <Link
-        href={{
-          pathname: Route.Routines,
-        }}
-      >
-        <a>Routines</a>
-      </Link>
-    </div>
+    <main className="pt-20">
+      <Logo />
+      <Temp />
+    </main>
   )
 }
 
