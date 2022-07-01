46c9cd9983bd1eb33cd69cf8119d3b0dc25f93c5
Ethan Mick <ethan@ethanmick.com>
2022-07-01 11:43:22 -0400

    Updated login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,35 +1,62 @@
+import { MailIcon } from '@heroicons/react/outline'
+import { Logo } from 'components'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { Route } from 'lib'
+import { workerData } from 'worker_threads'
 
 type FormData = {
   email: string
-  username: string
-  password: string
 }
 
-const Login: NextPage = () => {
-  const router = useRouter()
-  const credentialMode = router.query['credential_mode']
+type AwaitProps = {
+  email: string
+  onUndo: () => void
+}
+
+const AwaitConfirmation = ({ email, onUndo }: AwaitProps) => {
+  return (
+    <div className="flex flex-col gap-4 max-w-md">
+      <h1 className="text-center text-4xl">Waiting for Confirmation</h1>
+      <h2 className="text-lg">
+        Please do not close this window until opening the email link.
+      </h2>
+      <p>
+        We just sent an email to <span className="font-bold">{email}</span> (
+        <button className="underline" onClick={onUndo}>
+          undo
+        </button>
+        ). Please click the link in the email to login.
+      </p>
+    </div>
+  )
+}
+
+type LoginFormProps = {
+  onSuccess: (email: string) => void
+}
+
+/**
+ * Login form owns the login process up until the user successfully submits a
+ * valid email. Once we send the magic link we switch awaiting for confirmation.
+ */
+const LoginForm = ({ onSuccess }: LoginFormProps) => {
+  const [error, setError] = useState('')
   const { register, handleSubmit } = useForm<FormData>({
     defaultValues: {
       email: '',
-      username: '',
-      password: '',
     },
   })
 
-  const onSubmit = async (creds: FormData) => {
-    console.log(creds)
-
+  const onSubmit = async ({ email }: FormData) => {
     try {
       const res = await fetch(
         `${process.env.NEXT_PUBLIC_API_ROOT}/login?redirect_to=${window.location.origin}`,
         {
           method: 'POST',
           credentials: 'include',
-          body: JSON.stringify(creds),
+          body: JSON.stringify({ email }),
           headers: {
             'Content-Type': 'application/json',
           },
@@ -37,42 +64,63 @@ const Login: NextPage = () => {
       )
       if (res.status >= 400) {
         console.error(res)
+        setError((await res.json()) || `Sorry, that didn't work.`)
         return
       }
-      alert("Look for your log in link in your inbox!")
-    } catch (err) {
-      console.error(err)
+      onSuccess(email)
+    } catch (err: any) {
+      setError(err?.message)
     }
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {!credentialMode && (
-        <>
-          <input
-            {...register('email')}
-            className="bg-gray-700 border"
-            type="email"
-          />
-        </>
-      )}
-      {credentialMode && (
-        <>
-          <input
-            {...register('username')}
-            className="bg-gray-700 border"
-            type="text"
-          />
-          <input
-            {...register('password')}
-            className="bg-gray-700 border"
-            type="password"
-          />
-        </>
-      )}
+    <>
+      <h1 className="text-4xl">
+        Login to <Logo />
+      </h1>
+      <form
+        className="w-full px-4 md:px-0 md:w-[300px] flex flex-col items-center gap-8"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <input
+          {...register('email')}
+          required
+          autoFocus
+          className="bg-gray-700 border w-full"
+          placeholder="Email Address"
+          type="email"
+        />
+        <button
+          className="inline-flex items-center border font-medium px-4 py-2 w-full"
+          type="submit"
+        >
+          <MailIcon className=" -ml-1 mr-2 w-5 h-5" />
+          <span className="text-center w-full">Login with Email</span>
+        </button>
+      </form>
+    </>
+  )
+}
 
-      <button type="submit">Login</button>
-    </form>
+const Login: NextPage = () => {
+  const [email, setEmail] = useState('')
+  const [sent, setSent] = useState(false)
+
+  const onSend = (email: string) => {
+    setEmail(email)
+    setSent(true)
+  }
+
+  const undo = () => setSent(false)
+
+  return (
+    <main className="mt-20 md:mt-64 flex flex-col items-center gap-12">
+      {sent ? (
+        <AwaitConfirmation email={email} onUndo={undo} />
+      ) : (
+        <LoginForm onSuccess={onSend} />
+      )}
+    </main>
   )
 }
 
