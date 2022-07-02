d9d6436909ff6f953cfe9bc60c4a186719f42ee4
victorlora <3lorav+github@gmail.com>
2022-07-01 21:48:32 -0400

    ✨ Home page includes link to Exercises

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import { Logo } from 'components'
 import { Route } from 'lib'
 import type { NextPage } from 'next'
+import { Header, PageTitle } from 'components'
+import { Main } from 'components/layout'
+import { Menu } from 'components/menu'
 import Link from 'next/link'
 
 const LogoSection = () => (
