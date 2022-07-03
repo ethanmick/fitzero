6af22a4aa3b96d42e7b4824150d4c929a045fdff
Ethan Mick <ethan@ethanmick.com>
2022-07-03 16:14:26 -0400

    Format `authorize` page

--- pages/authorize.tsx
+++ pages/authorize.tsx
@@ -4,21 +4,19 @@ import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
-
 const AuthorizePage: NextPage = () => {
   const router = useRouter()
-  const token =router?.query['token']
-  const email =router?.query['email']
+  const token = router?.query['token']
+  const email = router?.query['email']
 
   useEffect(() => {
     if (!token) return
-
-    (async function() {
+    ;(async function () {
       try {
         const res = await fetch(
           `${process.env.NEXT_PUBLIC_API_ROOT}/authorize?token=${token}&email=${email}`,
           {
-            credentials: 'include'
+            credentials: 'include',
           }
         )
 
@@ -31,9 +29,8 @@ const AuthorizePage: NextPage = () => {
     })()
   }, [router, token, email])
 
-
   return (
-    <main className="mt-20 md:mt-64 flex flex-col items-center gap-12">
+    <main className="mt-20 flex flex-col items-center gap-12 md:mt-64">
       <h1 className="text-4xl">
         Logging in to <Logo />
       </h1>
