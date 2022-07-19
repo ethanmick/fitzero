26cdc894e9476dff13ec6e1472035b38d558d00d
victorlora <3lorav+github@gmail.com>
2022-07-18 22:48:13 -0400

    🚸 Add ability to filter exercise list

--- pages/exercises/index.tsx
+++ pages/exercises/index.tsx
@@ -11,6 +11,7 @@ import { Header } from 'components/header'
 import { Menu } from 'components/menu'
 import { ExercisesQuery } from 'lib/generated'
 import { gql } from '@apollo/client'
+import { useState } from 'react'
 
 const exercisesPageQueryDocument = gql`
   query Exercises {
@@ -44,14 +45,34 @@ const ExerciseItem = ({ id, name }: Exercise) => (
 
 type Props = InferGetServerSidePropsType<typeof getServerSideProps>
 const ExercisesPage: NextPage<Props> = ({ exercises }: Props) => {
+  const [filteredExercises, setFilteredExercises] = useState(exercises)
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const handleSearch = (query: string) => {
+    setSearchQuery(query)
+    const filtered = exercises.filter((item) => {
+      return item.name.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    })
+
+    setFilteredExercises(filtered)
+  }
+
   return (
     <Main>
       <Header right={<Menu />}>
         <PageTitle>Exercises</PageTitle>
       </Header>
+      <input
+        className="block w-full border-gray-300 bg-neutral-800 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-xl"
+        type="text"
+        name="search"
+        placeholder="Search"
+        value={searchQuery}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
       <ul className="divide-y">
-        {exercises.map((e) => (
-          <ExerciseItem key={e.id} {...e} />
+        {filteredExercises.map((exercise) => (
+          <ExerciseItem key={exercise.id} {...exercise} />
         ))}
       </ul>
     </Main>
