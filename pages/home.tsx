76a33c3be358d27fc468ff76a0fc876066b2038d
victorlora <3lorav+github@gmail.com>
2022-07-20 21:55:07 -0400

    Simple home page

---
+++ pages/home.tsx
@@ -0,0 +1,46 @@
+import { Header, Main, Menu, PageTitle } from 'components'
+import { Route } from 'lib'
+import type { NextPage } from 'next'
+import Link from 'next/link'
+
+const Home: NextPage = () => {
+  return (
+    <Main>
+      <Header right={<Menu />}>
+        <PageTitle>Home</PageTitle>
+      </Header>
+      <div className="bg-neutral-900">
+        <Link
+          href={{
+            pathname: Route.Exercises,
+          }}
+          className="block py-8"
+        >
+          Exercises
+        </Link>
+      </div>
+      <div className="bg-neutral-900">
+        <Link
+          href={{
+            pathname: Route.Routines,
+          }}
+          className="block py-8"
+        >
+          Routines
+        </Link>
+      </div>
+      <div className="bg-neutral-900">
+        <Link
+          href={{
+            pathname: Route.Workouts,
+          }}
+          className="block py-8"
+        >
+          Workouts
+        </Link>
+      </div>
+    </Main>
+  )
+}
+
+export default Home
