2eb0f9cc7501f5833b9827d15a3a647edeadfa03
victorlora <3lorav+github@gmail.com>
2022-07-21 23:44:36 -0400

    🚸 OnFocus in Exercise Log select all input text
    to facilitate updating values

--- pages/exerciselogs/new.tsx
+++ pages/exerciselogs/new.tsx
@@ -7,7 +7,6 @@ import {
 import Head from 'next/head'
 import { query, Route } from 'lib'
 import {
-  CreateExerciseLogInput,
   CreateExerciseLogMutation,
   CreateExerciseLogMutationVariables,
   ExerciseQuery,
@@ -15,11 +14,10 @@ import {
 } from 'lib/generated'
 import { gql, useMutation } from '@apollo/client'
 import { MinusIcon } from '@heroicons/react/outline'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { v4 as uuid } from 'uuid'
 import router from 'next/router'
 import { useForm } from 'react-hook-form'
-import { appendFile } from 'fs'
 
 const fetchExerciseQuery = gql`
   query ExerciseForNewExerciseLog($exerciseId: ID!) {
@@ -146,6 +144,9 @@ const Sets = ({
                     reps: e.target.value,
                   })
                 }
+                onFocus={(e) => {
+                  e.target.select()
+                }}
                 placeholder={'0'}
                 className="block w-full border-gray-300 bg-neutral-800 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm"
               />
@@ -163,6 +164,9 @@ const Sets = ({
                     weight: e.target.value,
                   })
                 }
+                onFocus={(e) => {
+                  e.target.select()
+                }}
                 className="block w-full border-gray-300 bg-neutral-800 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm"
               />
               <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
